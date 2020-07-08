29ae7eba9eb685f61b1d311a07e56e4ecfda4ac8
Bokul827 <68017472+Bokul827@users.noreply.github.com>
2020-07-08 20:35:23 +0600

    Update 1test.R

--- ML-workbook/1test.R
+++ ML-workbook/1test.R
@@ -1,6 +1,7 @@
 # setwd("~/datasets/titanic")
 
-train <- read.csv('https://raw.githubusercontent.com/raqueeb/mltraining/master/ML-workbook/train.csv')
+https://raw.githubusercontent.com/Bokul827/mltraining/master/ML-workbook/train.csv
+train <- read.csv('https://raw.githubusercontent.com/Bokul827/mltraining/master/ML-workbook/train.csv')
 # train <- read.csv("~/datasets/titanic/train.csv")
 # test <- read.csv("~/datasets/titanic/test.csv")
 test <- read.csv('https://raw.githubusercontent.com/raqueeb/mltraining/master/ML-workbook/test.csv')
